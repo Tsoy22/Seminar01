c19b6003ca6b53e40ad2ce042c117e525fd23795
Vitaly Tsoy <tsoyvitalik@icloud.com>
2022-09-06 20:59:27 +0300

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, Которая на вход принимает число и выдает его кваадрат
+// число уноженное на само себя
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен = {sqr1}");
